9404648dbe62fe4a344b4cd9e97dbd60362e2f32
Niamh <niamh.gowran2@mail.dcu.ie>
2022-08-07 16:58:01 +0100

    Household POST request

--- sustain/src/components/AddHouseEmissions.jsx
+++ sustain/src/components/AddHouseEmissions.jsx
@@ -5,9 +5,38 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Emissions.css';
 import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+
 
 function AddHouseEmissions() {
-  const[total, setTotal]= useState("")
+  const[total, setTotal]= useState("");
+  const[recyleMetal, setMetal]= useState("");
+  const[recylePlastic, setPlastic]= useState("") ;
+  const[recyleGlass, setGlass]= useState("") ;
+  const[recyleMagazines, setMagazines]= useState("");
+  const[numberOfPeople, setNumPeople]= useState("");
+  const[countryIsoCode, setCountryIso]= useState("");
+  const access_token = "00c112e599ff4c85bad0cfdacd3bb795";
+
+  const handleClick=(e)=>{
+    const emissions = {recyleMetal, recylePlastic, recyleGlass, recyleMagazines, numberOfPeople, countryIsoCode}
+    console.log(emissions)
+    axios
+        .post('https://api.sustain.life/v1/personal-calculator/household',
+         {emissions},
+          { headers: {
+          'Ocp-Apim-Subscription-Key': "00c112e599ff4c85bad0cfdacd3bb795"
+         }})
+        .then(res => {
+            alert(`Your total household emissions are: ${res.data.totalHouseholdWasteEmissionsCO2e}`)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+  };
+
+
 
   return (
     <>
@@ -17,96 +46,96 @@ function AddHouseEmissions() {
       <div class="col-sm">
       <InputGroup className="mb-3  ">
         <DropdownButton
-          variant="outline-secondary"
+          variant="outline-warning"
           title="Recyle Metal"
+          id="input-group-dropdown-1"
+          onSelect={(e)=>setMetal(e)}
+          
+        >
+          <Dropdown.Item eventKey="true">True</Dropdown.Item>
+          <Dropdown.Item eventKey="false">False</Dropdown.Item>
+        </DropdownButton>
+      </InputGroup>
+      </div>
+
+      <div class="col-sm">
+      <InputGroup className="mb-3">
+        <DropdownButton
+          variant="outline-warning"
+          title="Recyle Plastic"
+          id="input-group-dropdown-1"
+          onSelect={(e)=>setPlastic(e)}
+        >
+          <Dropdown.Item eventKey="true">True</Dropdown.Item>
+          <Dropdown.Item eventKey="false">False</Dropdown.Item>
+        </DropdownButton>
+      </InputGroup>
+      </div>
+
+      <div class="col-sm">
+      <InputGroup className="mb-3">
+        <DropdownButton
+          variant="outline-warning"
+          title="Recyle Glass"
+          id="input-group-dropdown-1"
+          onSelect={(e)=>setGlass(e)}>
+
+          <Dropdown.Item eventKey="true">True</Dropdown.Item>
+          <Dropdown.Item eventKey="false">False</Dropdown.Item>
+        </DropdownButton>
+      </InputGroup>
+      </div>
+      </div>
+
+      <div class="row">
+      <div class="col-sm">
+      <InputGroup className="mb-3">
+        <DropdownButton
+          variant="outline-warning"
+          title="Recyle Magazines"
+          id="input-group-dropdown-1"
+          onSelect={(e)=>setMagazines(e)}
+        >
+          <Dropdown.Item eventKey="true">True</Dropdown.Item>
+          <Dropdown.Item eventKey="false">False</Dropdown.Item>
+        </DropdownButton>
+      </InputGroup>
+      </div>
+
+      <div class="col-sm">
+      <InputGroup className="mb-3">
+        <DropdownButton
+          variant="outline-warning"
+          title="Enter Number of People"
           id="input-group-dropdown-1"
           
         >
-          <Dropdown.Item href="#">True</Dropdown.Item>
-          <Dropdown.Item href="#">False</Dropdown.Item>
+           <Form.Control onChange={(e)=>setNumPeople(e.target.value)} aria-label="Text input with dropdown button" />
         </DropdownButton>
       </InputGroup>
       </div>
 
       <div class="col-sm">
-      <InputGroup className="mb-3">
-        <DropdownButton
-          variant="outline-secondary"
-          title="Recyle Plastic"
-          id="input-group-dropdown-1"
-        >
-          <Dropdown.Item href="#">True</Dropdown.Item>
-          <Dropdown.Item href="#">False</Dropdown.Item>
-        </DropdownButton>
-      </InputGroup>
-      </div>
-
-      <div class="col-sm">
-      <InputGroup className="mb-3">
-        <DropdownButton
-          variant="outline-secondary"
-          title="Recyle Glass"
-          id="input-group-dropdown-1">
-          <Dropdown.Item href="#">True</Dropdown.Item>
-          <Dropdown.Item href="#">False</Dropdown.Item>
-        </DropdownButton>
-      </InputGroup>
-      </div>
-      </div>
-
-      <div class="row">
-      <div class="col-sm">
-      <InputGroup className="mb-3">
-        <DropdownButton
-          variant="outline-secondary"
-          title="Recyle Glass"
-          id="input-group-dropdown-1"
-        >
-          <Dropdown.Item href="#">True</Dropdown.Item>
-          <Dropdown.Item href="#">False</Dropdown.Item>
-        </DropdownButton>
-      </InputGroup>
-      </div>
-
-      <div class="col-sm">
-      <InputGroup className="mb-3">
-        <DropdownButton
-          variant="outline-secondary"
-          title="Recyle Magazines"
-          id="input-group-dropdown-1"
-        >
-          <Dropdown.Item href="#">True</Dropdown.Item>
-          <Dropdown.Item href="#">False</Dropdown.Item>
-        </DropdownButton>
-      </InputGroup>
-      </div>
-
-      <div class="col-sm">
-      <InputGroup className="mb-3">
-        <DropdownButton
-          variant="outline-secondary"
-          title="Number of People"
-          id="input-group-dropdown-1"
-        >
-           <Form.Control aria-label="Text input with dropdown button" />
-        </DropdownButton>
-      </InputGroup>
-      </div>
-      </div>
-
-      <div class="row">
       <InputGroup className="mb-3 text-center">
         <DropdownButton
-          variant="outline-secondary"
+          variant="outline-warning"
           title="Country Iso Code"
           id="input-group-dropdown-1"
+          onSelect={(e)=>setCountryIso(e)}
         >
-          <Dropdown.Item href="#">True</Dropdown.Item>
-          <Dropdown.Item href="#">False</Dropdown.Item>
+          <Dropdown.Item  eventKey="usa">usa</Dropdown.Item>
+          <Dropdown.Item  eventKey="uk">uk</Dropdown.Item>
         </DropdownButton>
       </InputGroup>
       </div>
+
       </div>
+
+      <Button variant="warning" type="submit" onClick={handleClick}>
+        Submit
+      </Button>
+      </div>
+      
     </>
   );
 }
